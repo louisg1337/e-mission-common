be586e195995d0671792f19d378fb37457e9c266
louisg1337 <louisgrassi1337@gmail.com>
2024-07-22 11:29:42 -0400

    Added in constants for kWh and kg of CO2, which were found on .gov websites

--- src/emcommon/metrics/footprint_calculations.py
+++ src/emcommon/metrics/footprint_calculations.py
@@ -6,10 +6,20 @@ energy usage (kwh) and carbon emissions (kg_co2).
 # https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
 KG_CO2_PER_GALLON_GASOLINE = 8.89
 KG_CO2_PER_GALLON_DIESEL = 10.18
+KG_CO2_PER_GALLON_BIODIESEL = KG_CO2_PER_GALLON_DIESEL * .26 # https://afdc.energy.gov/fuels/biodiesel-benefits
+KG_CO2_PER_GALLON_LPG = 5.75 # https://www.eia.gov/environment/emissions/co2_vol_mass.php
+KG_CO2_PER_GALLON_CNG = KG_CO2_PER_GALLON_GASOLINE * 1.22 # https://www.ctc-n.org/technology-library/vehicle-and-fuel-technologies/compressed-natural-gas-cng-fuel
+KG_CO2_PER_KG_HYDROGEN = 0 
+KG_CO2_PER_KWH_ELECTRICITY = 0.5 # Figure out way to integrate this with eGrid work
 
 DIESEL_GGE = 1.136 # from energy.gov
 KWH_PER_GALLON_GASOLINE = 33.7 # from the EPA, used as the basis for MPGe
 KWH_PER_GALLON_DIESEL = KWH_PER_GALLON_GASOLINE * 1.14
+# GGE constants found from https://epact.energy.gov/fuel-conversion-factors
+KWH_PER_GALLON_BIODIESEL = KWH_PER_GALLON_GASOLINE * 1.05 
+KWH_PER_GALLON_LPG = KWH_PER_GALLON_GASOLINE * .74
+KWH_PER_GALLON_CNG = KWH_PER_GALLON_GASOLINE * .26
+KWH_PER_KG_HYDROGEN = KWH_PER_GALLON_GASOLINE * 1.00
 
 KG_CO2_PER_KWH_GASOLINE = KG_CO2_PER_GALLON_GASOLINE / KWH_PER_GALLON_GASOLINE
 KG_CO2_PER_KWH_DIESEL = KG_CO2_PER_GALLON_DIESEL / KWH_PER_GALLON_DIESEL
