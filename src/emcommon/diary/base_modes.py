02bf7c8fefeee7793d6be4f222031f2a94404eeb
Jack Greenlee <greenljk@mail.uc.edu>
2024-08-01 19:44:46 -0400

    add Light Rail to "TRAIN" base mode
    Although we have a separate base mode for Light Rail, it isn't used in the default set of mode label options. So in the default configuration, "Train" should be all-encompassing of light, hybrid, and heavy rail.

--- src/emcommon/diary/base_modes.py
+++ src/emcommon/diary/base_modes.py
@@ -175,7 +175,7 @@ BASE_MODES = {
       "icon": 'train-car-passenger',
       "color": mode_colors['periwinkle'],
       "met": NON_ACTIVE_METS,
-      "footprint": { "transit": ["HR", "YR"] } # heavy rail, hybrid rail
+      "footprint": { "transit": ["LR", "HR", "YR"] } # light rail, heavy rail, hybrid rail
   },
   "TRAM": {
       "icon": 'tram',
