6d2c0178b54bdb8a935cb1669e2667a581959958
Jack Greenlee <greenljk@mail.uc.edu>
2024-08-02 08:20:45 -0400

    add -f option to compile_to_js.sh
    "force compile" / "fresh compile"

--- bin/compile_to_js.sh
+++ bin/compile_to_js.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# if '-f' (force compile), remove the old files before compiling
+if [[ " $* " == *" -f "* ]]; then
+    rm -rf ./emcommon_js/*
+fi
 transcrypt --nomin --ecom --xreex ./src/index.py -od ../emcommon_js
