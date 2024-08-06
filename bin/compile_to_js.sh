a3046645268d3b1c0e53182c19fc218e7373401b
Jack Greenlee <greenljk@mail.uc.edu>
2024-08-05 23:45:44 -0400

    enhance compile_to_js.sh; add -f and echos
    -f switch for "force compile" / "fresh compile"
    set -e to stop if a command fails
    echos to make it clearer what it is doing

--- bin/compile_to_js.sh
+++ bin/compile_to_js.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+
+set -e
+
+# if '-f' (force compile), remove the old files before compiling
+if [[ " $* " == *" -f "* ]]; then
+    echo "-- Removing old files..."
+    rm -rf ./emcommon_js/*
+fi
+
+echo "-- Compiling Python to JavaScript..."
 transcrypt --nomin --ecom --xreex ./src/index.py -od ../emcommon_js
+
+echo "-- Generating TypeScript declaration files..."
 npx tsc
+
+echo "Done"
